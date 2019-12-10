de7c0c00ee17282ad29da94b837f90ff63747aa2
Davide Cernuto <58591714+BakaDavi@users.noreply.github.com>
2019-12-10 19:27:51 +0100

    Day 1 part 2
    The input works the same way as in part 1, now the program considers fuel's mass.

---
+++ Day1p2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Day1p2 {
+    public static void main(String[] args){
+        int result = 0;
+        System.out.println("Input mass:");
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextInt()){
+            int mass = scanner.nextInt();
+            while(fuel(mass) > 0){
+                result += fuel(mass);
+                mass = fuel(mass);
+            }
+        }
+        scanner.close();
+        System.out.println("Fuel needed: " +result);
+    }
+    private static int fuel(int m){
+        return (int) Math.floor((m/3)-2);
+    }
+}
